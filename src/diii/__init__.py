a64d639753f87e532bfa9d789dd635d82951de52
tehn <tehn@nnnnnnnn.co>
2025-12-20 15:06:16 -0500

    pkg_resouces deprecated

--- src/diii/__init__.py
+++ src/diii/__init__.py
@@ -1,9 +1,3 @@
-import pkg_resources
-
 from setuptools_scm import get_version
 
-try:
-    __version__ = get_version()
-except LookupError:
-    from pkg_resources import get_distribution
-    __version__ = get_distribution("monome-diii").version
+__version__ = get_version()
