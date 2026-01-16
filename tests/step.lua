e67f13d8681d42927c26045c2131d079b52bf307
tehn <tehn@nnnnnnnn.co>
2026-01-15 20:18:06 -0500

    fix version

--- tests/step.lua
+++ tests/step.lua
@@ -1,5 +1,7 @@
 print("step")
 
+alt = 0
+
 ch = 0
 step = 1
 last = 0
@@ -8,22 +10,27 @@ map = {66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96}
 
 tick = function()
 	if last > 0 then midi_note_off(map[last]) end
-	step = (step % 16) + 1
+  if cut then
+    step = cut
+    cut = nil
+  else
+    step = (step % 16) + 1
+  end
 	last = note[step]
 	if last > 0 then midi_note_on(map[last]) end
 	redraw()
 end
 
-metro = function(index, count)
-	tick()
-end
+m = metro.init(tick, .1)
 
-grid = function(x,y,z)
-	if z==0 then return end
-	if y==0 then
-		-- cut
-	else
-		x = x+1
+event_grid = function(x,y,z)
+	if y==1 then
+		if x==16 then
+      alt = z
+    end
+  elseif y==2 and z==1 then
+    cut = x
+	elseif z == 1 then
 		if note[x] == y then note[x] = 0
 		else note[x] = y end
 		redraw()
@@ -32,10 +39,10 @@ end
 
 redraw = function()
 	grid_led_all(0)
-	grid_led(step-1,0,5)
-	for n=1,16 do
+	grid_led(step,2,5)
+	for n=3,16 do
 		if note[n] > 0 then
-			grid_led(n-1,note[n],step==n and 15 or 5)
+			grid_led(n,note[n],step==n and 15 or 5)
 		end
 	end
 	grid_refresh()
@@ -53,6 +60,6 @@ midi_rx = function(d1,d2,d3,d4)
 	--print("midi",d1,d2,d3,d4)
 end
 
---metro_set(1, 150)
-
 redraw()
+
+m:start()
